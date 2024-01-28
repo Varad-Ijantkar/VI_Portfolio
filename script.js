d89dd6bba56f2146565e60939ce77ff2e4918d33
VaradIjanktar <155927031+VaradIjanktar@users.noreply.github.com>
2024-01-28 18:57:51 +0530

    Main

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Front-end Developer","Software Engineer","Ethical Hacker"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
